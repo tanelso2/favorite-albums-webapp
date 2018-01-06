e5e1502e08515f1855f650ca6d76bd5fe2d6781a
Thomas Nelson <tanelso2@gmail.com>
2018-01-05 21:24:56 -0800

    Added grouping capabilities

--- frontend/src/frontend/core.cljs
+++ frontend/src/frontend/core.cljs
@@ -32,11 +32,28 @@
     (str/trim)))
 
 (defn sort-key-fn [sort-key]
+  "Wow this needs a better name"
   (fn [x]
     (-> x
-        (get sort-key)
+        sort-key
         remove-preceding-articles
-        str/lower-case)))
+        str/upper-case)))
+
+(defn sort-group [sort-fn [group-key group]]
+  [group-key (sort-by sort-fn group)])
+
+(defn get-album-groups [grouping sorting albums]
+  "Wow this function looks horrendous"
+  (let [sort-fn (case grouping
+                  :alphabet (sort-key-fn sorting)
+                  :artist (sort-key-fn :album-name))]
+    (->>
+      (case grouping
+        :alphabet (group-by (comp first remove-preceding-articles sorting) albums)
+        :artist (group-by :artist-name albums))
+      (vec)
+      (sort-by (sort-key-fn first))
+      (mapv (partial sort-group sort-fn)))))
 
 ;; -------------------------
 ;; State
@@ -44,6 +61,8 @@
 (defonce albums (r/atom nil))
 
 (defonce sorting (r/atom :artist-name))
+
+(defonce grouping (r/atom nil))
 
 ;; -------------------------
 ;; Views
@@ -65,6 +84,15 @@
    (for [album albums-data]
      ^{:key album} [:div.col-md-3.col-sm-5 [album-display album]])])
 
+(defn album-groups [groupings]
+  [:div
+    (for [[group-key group] groupings]
+      ^{:key group-key}
+      [:div
+       [:div.row
+        [:h2.group-title group-key]]
+       [album-list group]])])
+
 (defn exclusive-button-group-button [button-title button-state state-atom]
   (let [checked (= button-state @state-atom)]
     [:label.btn.btn-secondary
@@ -83,17 +111,28 @@
       ^{:key state} [exclusive-button-group-button title state state-atom])])
 
 (defn sorting-buttons [sorting-atom]
-  [:div.row.sorting-buttons
-    [:label.btn [:strong "Sort by:"]]
+  [:div.row.control-buttons
+    [:label.btn.col-1 [:strong "Sort by:"]]
     [exclusive-button-group sorting-atom
      ["Title" :album-name]
      ["Artist" :artist-name]]])
+
+(defn grouping-buttons [grouping-atom]
+  [:div.row.control-buttons
+   [:label.btn.col-1 [:strong "Group by:"]]
+   [exclusive-button-group grouping-atom
+    ["None" nil]
+    ["Alphabet" :alphabet]
+    ["Artist" :artist]]])
 
 (defn app-entry []
   [:div.container-fluid
    [:h1 "Thomas's Favorite Albums"]
    [sorting-buttons sorting]
-   [album-list (sort-by (sort-key-fn @sorting) @albums)]
+   [grouping-buttons grouping]
+   (if (nil? @grouping)
+     [album-list (sort-by (sort-key-fn @sorting) @albums)]
+     [album-groups (get-album-groups @grouping @sorting @albums)])
    [:p "View the code on "
       [:a {:href "https://github.com/tanelso2/favorite-albums-webapp"}
        "GitHub"]]])
