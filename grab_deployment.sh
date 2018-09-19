ee265d97f217b853004a66f7bde6a20126c64f85
Thomas Nelson <tanelso2@gmail.com>
2018-09-18 19:52:53 -0700

    Updated deployment to be more general. Will use this to use on my website

--- grab_deployment.sh
+++ grab_deployment.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-rm deployment.tar.gz || true
+FILE_LOCATION=${1?"Error! Download location not specified"}
 
 curl -s https://api.github.com/repos/tanelso2/favorite-albums-webapp/releases/latest \
 | grep "browser_download_url" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -qi -
-
-tar -zxf deployment.tar.gz -C /www/landing_page/favorites/albums
+| wget -qi - -O $FILE_LOCATION
