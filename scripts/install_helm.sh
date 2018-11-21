a4081459979c0737fc58698f194dd060d4fb0d50
Thomas Nelson <tanelso2@gmail.com>
2018-11-20 20:07:33 -0800

    Helm tar

--- scripts/install_helm.sh
+++ scripts/install_helm.sh
@@ -4,5 +4,5 @@ set -e
 set -x
 
 wget https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64.tar.gz
-tar -zxf helm-v2.11.0-amd64.tar.gz
+tar -zxf helm-v2.11.0-linux-amd64.tar.gz
 cp linux-amd64/helm /usr/local/bin/helm
