b765dee10626e39c99f544d63d957a993c6aea81
Thomas Nelson <tanelso2@gmail.com>
2018-11-20 20:01:00 -0800

    Gahh

---
+++ scripts/install_helm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+set -x
+
+wget https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64.tar.gz
+tar -zxf helm-v2.11.0-amd64.tar.gz
+cp linux-amd64/helm /usr/local/bin/helm
